276e0d35a5aebd9ba12cde03e8aae6026eff0564
Mr-Painter <571928107@qq.com>
2024-12-31 13:46:04 +0800

    add  empty template project

---
+++ Code/Application/src/main.c
@@ -0,0 +1,17 @@
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+int main(void)
+{
+
+	while(1);
+}
+
+void vApplicationStackOverflowHook(TaskHandle_t xTask,
+                                   char *pcTaskName)
+{
+	
+}
+
+
